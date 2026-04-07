244e30acc68fd6d3e9724020c4a81159b8a95afc
triciamei <mamarilsakura@gmail.com>
2026-04-07 20:58:13 +0800

    Initial commit - Activity 2 calculator

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Choose operation (+, -, *, /): ");
+        char op = Convert.ToChar(Console.ReadLine());
+
+        double result = 0;
+
+        if (op == '+')
+            result = num1 + num2;
+        else if (op == '-')
+            result = num1 - num2;
+        else if (op == '*')
+            result = num1 * num2;
+        else if (op == '/')
+            result = num1 / num2;
+        else
+            Console.WriteLine("Invalid operator");
+
+        Console.WriteLine("Result: " + result);
+    }
+}
