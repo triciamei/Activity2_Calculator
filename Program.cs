a80c3436bfbf67faf18605060054f0c84ed8f253
triciamei <mamarilsakura@gmail.com>
2026-04-07 23:01:47 +0800

    Added message to demonstrate feature branch

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main()
     {
+        Console.WriteLine("Hello from feature branch!");
+
         Console.Write("Enter first number: ");
         double num1 = Convert.ToDouble(Console.ReadLine());
 
